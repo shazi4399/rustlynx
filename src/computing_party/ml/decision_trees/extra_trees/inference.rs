dacea92c902a98244db8b2117ffcb06a47d8f177
Samuel Adams <samuel.adams.d@gmail.com>
2021-03-23 15:39:22 -0700

    Added rng functions

--- src/computing_party/ml/decision_trees/extra_trees/inference.rs
+++ src/computing_party/ml/decision_trees/extra_trees/inference.rs
@@ -3,6 +3,6 @@ use super::super::super::super::Context;
 use super::extra_trees;
 
 pub fn run(ctx: &mut Context) -> Result<(), Box<dyn Error>> {
-    let (xt_ctx, data) = extra_trees::init(&ctx.ml.cfg)?;
+    let (xt_ctx, data, classes) = extra_trees::init(&ctx.ml.cfg)?;
     Ok(())
 }
