d908542648b6caeb15c87548cb2f7b81bdce40c5
David <David>
2021-04-12 18:14:55 -0700

    ditto

--- toml_editor.py
+++ toml_editor.py
@@ -17,24 +17,24 @@ party=args[6]
 
 with open(toml_learn_path, 'w') as file:
     file.write("class_label_count = 2\n")
-    file.write("attribute_count = %s\n".format(attr))
-    file.write("instance_count = %s\n".format(rows))
-    file.write("tree_count = %s\n".format(trees))
-    file.write("max_depth = %s\n".format(depth))
-    file.write("feature_count = %s\n".format(feat))
+    file.write("attribute_count = {}\n".format(attr))
+    file.write("instance_count = {}}\n".format(rows))
+    file.write("tree_count = {}}\n".format(trees))
+    file.write("max_depth = {}}\n".format(depth))
+    file.write("feature_count = {}}\n".format(feat))
     file.write("epsilon = 0.05\n")
-    file.write("data = data/Party%s_breast/%sX_train.csv\n".format(party, fold))
-    file.write("classes = data/Party%s_breast/%sy_train.csv\n".format(party, fold))
-    file.write("save_location = treedata/Party%s_trees.json".format(party))
+    file.write("data = data/Party{}}_breast/{}}X_train.csv\n".format(party, fold))
+    file.write("classes = data/Party{}}_breast/{}}y_train.csv\n".format(party, fold))
+    file.write("save_location = treedata/Party{}}_trees.json".format(party))
 
 with open(toml_inference_path, 'w') as file:
 
     file.write("class_label_count = 2\n")
-    file.write("attribute_count = %s\n".format(attr))
-    file.write("instance_count = %s\n".format(rows))
-    file.write("max_depth = %s\n".format(depth))
-    file.write("data = data/Party%s_breast/%sX_test.csv\n".format(party, fold))
-    file.write("classes = data/Party%s_breast/%sy_test.csv\n".format(party, fold))
-    file.write("save_location = treedata/Party%s_trees.json".format(party))
+    file.write("attribute_count = {}}\n".format(attr))
+    file.write("instance_count = {}}\n".format(rows))
+    file.write("max_depth = {}}\n".format(depth))
+    file.write("data = data/Party{}}_breast/{}}X_test.csv\n".format(party, fold))
+    file.write("classes = data/Party{}}_breast/{}}y_test.csv\n".format(party, fold))
+    file.write("save_location = treedata/Party{}}_trees.json".format(party))
 
 
