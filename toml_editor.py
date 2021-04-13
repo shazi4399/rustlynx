40db3b80193e4ad6ddf8e6ab631247e533bd3bc7
David <David>
2021-04-12 18:13:54 -0700

    toml fix

--- toml_editor.py
+++ toml_editor.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 party = 0
-toml_learn_path = "learning%s_temp.toml".format(party)
-toml_inference_path = "inference%s_temp.toml".format(party)
+toml_learn_path = "learning{}_temp.toml".format(party)
+toml_inference_path = "inference{}_temp.toml".format(party)
 
 args = sys.argv[1:]
 
