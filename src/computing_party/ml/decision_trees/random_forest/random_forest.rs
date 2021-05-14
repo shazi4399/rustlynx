40f3aac52f6e09f0bed6a057c932a18ffcbf0077
David <David>
2021-05-13 17:46:04 -0700

    quality of life change

--- src/computing_party/ml/decision_trees/random_forest/random_forest.rs
+++ src/computing_party/ml/decision_trees/random_forest/random_forest.rs
@@ -140,6 +140,9 @@ pub fn init(cfg_file: &String) -> Result<(RFContext, Vec<Vec<Wrapping<u64>>>, Ve
     let data = io::matrix_csv_to_wrapping_vec(&settings.get_str("data")?)?;
     let mut classes = io::matrix_csv_to_wrapping_vec(&settings.get_str("classes")?)?;
 
+    let instance_count = data.len(); // ADDED BY DAVID, hopefully won't cause issues. This relives huge headaches though
+    let attribute_count = data[0].len(); // ADDED BY DAVID, hopefully won't cause issues. This relives huge headaches though
+
     classes = util::transpose(&classes)?;
     let tc = TrainingContext {
         instance_count,
