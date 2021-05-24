a85f8447c6947d71bf2757145d86cdd589a3334f
David <David>
2021-05-23 17:05:12 -0700

    toml fix

--- toml_runtime_editor.py
+++ toml_runtime_editor.py
@@ -6,7 +6,7 @@ args = sys.argv[1:]
 test_size=args[0]
 party=args[1]
 
-toml_learn_path = "cfg/ml/test_DT_protocols/learning{}.toml".format(party)
+toml_learn_path = "cfg/ml/time_test/learning{}.toml".format(party)
 
 with open(toml_learn_path, 'w') as file:
     file.write("test_size = {}\n".format(test_size))
