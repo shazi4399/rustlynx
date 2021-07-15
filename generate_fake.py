0e02be7a80117dc7e44b20458c55bdaaefb846d2
David <David>
2021-07-14 17:47:56 -0700

    f

--- generate_fake.py
+++ generate_fake.py
@@ -18,10 +18,10 @@ folds = 5
 for k in range(folds):
     with open("/home/XT{}/data/Party{}_{}/{}X_train.csv".format(str(int(party) + 1), party, "fake", k + 1), 'w') as f:
         vals = [0] * cols
-        for r in rows:
+        for r in range(rows):
             f.write(",".join(vals) + "\n")
 
     with open("/home/XT{}/data/Party{}_{}/{}y_train.csv".format(str(int(party) + 1), party, "fake", k + 1), 'w') as f:
         vals = [0] * 2
-        for r in rows:
+        for r in range(rows):
             f.write(",".join(vals) + "\n")
